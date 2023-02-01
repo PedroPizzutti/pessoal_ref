4f0fef3d95c415106a4bc867eeb7998f5fb9be9c
Pedro Pizzutti <pedropizzutti@gmail.com>
2023-01-31 22:39:55 -0300

    configurado o banco de dados e criada a primeira tabela com sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'sqlite',
+  storage: './bd_pessoal_ref.sqlite',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+};
