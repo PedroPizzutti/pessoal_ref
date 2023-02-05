7c8737d407a6545c09c7a7d253de190222bbf2af
Pedro Pizzutti <87579876+PedroPizzutti@users.noreply.github.com>
2023-02-05 15:07:35 -0300

    criadas as migrações de livros e artigos

---
+++ api/src/database/migrations/20230205175641-artigos.js
@@ -0,0 +1,62 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('artigos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'usuarios',
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      autor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      titulo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ano: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      revista: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      volume: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      numero: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      paginacao: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('artigos');
+  },
+};
