4f47bab6bb4e377af1ebeea5564f21620e2405d6
Pedro Pizzutti <87579876+PedroPizzutti@users.noreply.github.com>
2023-02-19 18:15:37 -0300

    configurado prettier e eslint da aplicação

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import './App.css'
+import React from 'react';
 
-export default function App() {
-  return <h1>Hello world!</h1>
+function App() {
+  return <h1>Hello World!</h1>;
 }
+
+export default App;
