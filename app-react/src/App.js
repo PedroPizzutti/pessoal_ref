831d4722d84d424d78d24c87569ecdef99bbd330
Pedro Pizzutti <87579876+PedroPizzutti@users.noreply.github.com>
2023-02-20 18:55:08 -0300

    criado o header da aplicação

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -3,10 +3,12 @@ import GlobalStyles from './styles/GlobalStyles';
 
 // temp
 import Login from './pages/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
--- app-react/src/config/colors.js
+++ app-react/src/config/colors.js
@@ -1,5 +1,5 @@
 export const corPrincipal = '#F2C00C';
-export const corEscuraPrincipal = '#C29A0A';
+export const corEscuraPrincipal = '#5C501C';
 
 export const corSucesso = '#E8CE0C';
 export const corInformacao = '#DB9D15';
