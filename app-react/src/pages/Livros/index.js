879cd438616e87a5dae7d927a1d277635810470f
Pedro Pizzutti <pedropizzutti@gmail.com>
2023-03-12 23:12:47 -0300

    iniciado a criação da exibição dos livros do usuário

--- app-react/src/pages/Livros/index.js
+++ app-react/src/pages/Livros/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
-import { Titulo } from './styled';
+import { Tabela, Titulo } from './styled';
 
 export default function Livros() {
+  const [livros, setLivros] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      setIsLoading(true);
+      const response = await axios.get('/livros');
+      setLivros(response.data);
+      setIsLoading(false);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <Titulo>Livros</Titulo>
+      <Tabela>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Ano</th>
+            <th>Autor</th>
+            <th>Título</th>
+          </tr>
+        </thead>
+        <tbody>
+          {livros.map((livro) => (
+            <tr>
+              <td>{livro.id}</td>
+              <td>{livro.ano}</td>
+              <td>{livro.autor}</td>
+              <td>{livro.titulo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Tabela>
     </Container>
   );
 }
--- app-react/src/pages/Livros/styled.js
+++ app-react/src/pages/Livros/styled.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
+import * as cores from '../../config/colors';
 
-export const Titulo = styled.h1``;
+export const Titulo = styled.h1`
+  font-size: 28px;
+`;
+
+export const Tabela = styled.table`
+  width: 100%;
+  margin-top: 10px;
+  border-collapse: collapse;
+
+  th,
+  td {
+    border: 1px solid ${cores.corPrincipal};
+    align-items: center;
+    justify-content: left;
+    padding: 15px;
+  }
+
+  th {
+    font-size: 22px;
+  }
+
+  td {
+    font-size: 18px;
+  }
+`;
